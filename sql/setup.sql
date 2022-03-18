dac62e0ed3ea9d45cf4b063d6a76011217942d90
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-03-18 11:12:32 -0700

    added Bobs Quotes table to setup

--- sql/setup.sql
+++ sql/setup.sql
@@ -2,6 +2,7 @@
 -- The SQL in this file will be executed when you run `npm run setup-db`
 
 DROP TABLE IF EXISTS songs;
+DROP TABLE IF EXISTS bobs;
 
 CREATE TABLE songs (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -10,9 +11,25 @@ CREATE TABLE songs (
   album TEXT
 );
 
+CREATE TABLE bobs (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  quote TEXT NOT NULL,
+  character TEXT NOT NULL,
+  season INT NOT NULL,
+  episode INT NOT NULL,
+);
+
 INSERT INTO
   songs(title, artist, album)
 VALUES
   ('Honey Whiskey', 'SATICA', 'Honey Whiskey'),
   ('Industry Baby', 'Lil Nas X', 'Montero'),
   ('Bad Stuff Happens in the Bathroom', 'Bob''s Burgers', 'The Bob''s Burgers Music Album');
+
+INSERT INTO
+  bobs(quote, character, season, episode)
+VALUES
+  ('Wine helps me drink.', 'Linda Belcher', 4, 1),
+  ('You''ll take the butts you''re given, and you''ll like it!', 'Gene Blecher', 4, 13),
+  ('You''re a hurtful slut, Bob!', 'Linda Belcher', 2, 6),
+  ('I am powerful forces.', 'Tina Belcher', 7, 3);
