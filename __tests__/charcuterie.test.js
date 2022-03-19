3bf0cbc067be61d284f3c0e6591f3db653a1dd5b
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-03-19 14:59:23 -0700

    added getAll to Charcuteries

--- __tests__/charcuterie.test.js
+++ __tests__/charcuterie.test.js
@@ -2,7 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const Bob = require('../lib/models/Bob');
+const Charcuterie = require('../lib/models/Charcuterie');
 
 describe('alchemy-app routes', () => {
   beforeEach(() => {
@@ -21,15 +21,23 @@ describe('alchemy-app routes', () => {
       category: 'carb',
       cold: true
     };
- 
+
     const res = await request(app)
       .post('/api/v1/charcuteries')
       .send(expected);
- 
+
     expect(res.body).toEqual({ id: expect.any(String), ...expected });
   });
- 
+
   // getAll (res:get)
+  it('should get all charc rows', async () => {
+    const expected = await Charcuterie.getAllCharcs();
+
+    const res = await request(app)
+      .get('/api/v1/charcuteries');
+
+    expect(res.body).toEqual(expected);
+  });
   // getSingle(id) (id:1, res:get/exID)
   // patch(update) (const:insert, res:patch(link/id)/send(new), ex:id/string)
     // **patch return (await getId(const.id) toEq)**
