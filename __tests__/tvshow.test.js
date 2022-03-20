6315b23da3d2be20350192b551b0a0a3d6504eee
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-03-19 21:47:43 -0700

    added insert to TVShows

---
+++ __tests__/tvshow.test.js
@@ -0,0 +1,39 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('alchemy-app routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+
+  // create (ex:new, res:post(link)/send(ex), toEq:id/string, sprEx)
+  it('should create a new row in tvshow table', async () => {
+    const expected = 
+    {
+      title: 'Haikyuu!!',
+      genre: ['Animation', 'Comedy', 'Drama', 'Sports'],
+      seasons: 4,
+      episodes: 88,
+      runTime: 24,
+      active: false
+    };
+
+    const res = await request(app)
+      .post('/api/v1/tvshows')
+      .send(expected);
+
+    expect(res.body).toEqual({ id: expect.any(String), ...expected });
+  });
+  // getAll (res:get)
+  // getSingle(id) (id:1, res:get/exID, ret:sprEx)
+  // patch(update) (const:insert, res:patch(link/id)/send(new), ex:id/string)
+    // **patch return (await getId(const.id) toEq)
+  // delete (const:insert, res:delete(link/id), const:ALL)
+    // **del ret (ex:All.not.toContain(res.body))
+});
